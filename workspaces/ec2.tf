c1be924e0bc10df0c4bafc1148116e96dce30a28
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-12-09 20:46:53 +0000

    terraform

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "terraform" {
 
         ami = "ami-09c813fb71547fc4f"
-        instance_type = lookup(var.instance_type, terraform.workspace)
-        vpc_security_group_ids = ["sg-07f58d150b8b8711d"]
+        instance_type = lookup(var.instance_type, terraform.workspace)   #basically terraform.worksapce tells env
+        vpc_security_group_ids = ["sg-099a766437c0af022"]
         tags = {
             Name = "terraform-${terraform.workspace}"
         }
